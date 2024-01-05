ede31142afb5783717a387bbfff52438038372a7
Ayush Chauhan <chauhanayush7983@gmail.com>
2024-01-05 16:22:21 +0530

    Last Commit

--- lib/view/mainScreen.dart
+++ lib/view/mainScreen.dart
@@ -10,6 +10,8 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   String imgUrl = "";
+  int memeNo = 1;
+  bool isLoading = true;
 
   @override
   void initState() {
@@ -21,59 +23,82 @@ class _MainScreenState extends State<MainScreen> {
     String getImageUrl = await FetchMeme.fetchNewMeme();
     setState(() {
       imgUrl = getImageUrl;
+      isLoading = false;
     });
+  }
+
+  void IncMemeCount() {
+    memeNo++;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 100,
-            ),
-            Text(
-              "Meme #1",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Target 500 memes",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Image.network(imgUrl),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  UpdateImg();
-                },
-                child: Container(
-                    height: 50,
-                    width: 150,
-                    child: Center(
-                        child: Text(
-                      "More fun!!",
-                      style: TextStyle(fontSize: 20),
-                    )))),
-            Spacer(),
-            Text(
-              "App created by",
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              "Ayush Chauhan",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            )
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 162, 248, 164),
+              Color.fromARGB(255, 133, 226, 198)
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 130,
+              ),
+              Text("Meme #${memeNo.toString()}"),
+              SizedBox(
+                height: 20,
+              ),
+              isLoading
+                  ? Container(
+                      height: 30,
+                      width: 30,
+                      child: CircularProgressIndicator(),
+                    )
+                  : Image.network(imgUrl, height: 350, width: 320),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    UpdateImg();
+                    IncMemeCount();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  child: Ink(
+                      decoration: BoxDecoration(
+                          gradient: const LinearGradient(colors: [
+                            Color.fromARGB(255, 242, 175, 217),
+                            Color.fromARGB(255, 227, 245, 150)
+                          ]),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Container(
+                          width: 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'More Fun',
+                            style: TextStyle(fontSize: 18),
+                          )))),
+              Spacer(),
+              Text(
+                "App created by",
+                style: TextStyle(fontSize: 15),
+              ),
+              Text(
+                "Ayush Chauhan",
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+              )
+            ],
+          ),
         ),
       ),
     );
